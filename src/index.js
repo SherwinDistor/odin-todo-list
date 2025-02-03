2796c05ad5dd52614925f3eb594142eee0af3db8
sherwindistor <182566354+SherwinDistor@users.noreply.github.com>
2025-02-02 19:22:31 -0600

    Create dom loaders for header and main

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import './styles.css';
-import { ListItem } from './create-list-item.js';
-import { Project } from './create-project.js';
+// import { ListItem } from './create-list-item.js';
+// import { Project } from './create-project.js';
+import { loadHome } from './load-home.js';
 
-const itemOne = new ListItem(
-    'Do the dishes',
-    'Private fields can only be declared up-front in a field declaration. They cannot be created later through assigning to them, the way that normal properties can.',
-    1020,
-    'high',
-);
+// const itemOne = new ListItem(
+//     'Do the dishes',
+//     'Private fields can only be declared up-front in a field declaration. They cannot be created later through assigning to them, the way that normal properties can.',
+//     1020,
+//     'high',
+// );
 
-const chores = new Project('Chores');
+// const chores = new Project('Chores');
 
-chores.list.push(itemOne);
+// chores.list.push(itemOne);
 
-console.log(chores);
+// console.log(chores);
 
-console.log(chores.list[0].isComplete);
+// console.log(chores.list[0].isComplete);
+
+loadHome();
---
+++ src/load-home.js
@@ -0,0 +1,97 @@
+function loadHeader() {
+    const header = document.createElement('header');
+    header.classList = 'wrap';
+
+    const logo = document.createElement('a');
+    logo.id = 'logo-text';
+    logo.textContent = 'To Do List';
+
+    const newProjectBtn = document.createElement('button');
+    newProjectBtn.classList = 'new-project-btn';
+    newProjectBtn.textContent = 'Create New Project';
+
+    header.appendChild(logo);
+    header.appendChild(newProjectBtn);
+
+    return header;
+}
+
+function loadMain() {
+    const main = document.createElement('main');
+    main.classList = 'wrap';
+
+    const projectContainer = document.createElement('div');
+    projectContainer.classList = 'wrap';
+    projectContainer.id = 'project-container';
+
+    projectContainer.appendChild(loadList());
+    main.appendChild(projectContainer);
+
+    return main;
+}
+
+// Example list item
+function loadList() {
+    const list = document.createElement('div');
+    list.id = 'list';
+
+    const listHeader = document.createElement('h2');
+    listHeader.textContent = 'Groceries';
+
+    const listItems = document.createElement('ul');
+
+    const listItem = document.createElement('li');
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+
+    const listInfo = document.createElement('div');
+    listInfo.classList = 'list-info';
+
+    const listTitle = document.createElement('p');
+    listTitle.classList = 'list-item-title';
+    listTitle.textContent = 'Eggs';
+
+    const listDescription = document.createElement('p');
+    listDescription.classList = 'list-item-description';
+    listDescription.textContent =
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit.';
+
+    const listFooter = document.createElement('div');
+    listFooter.classList = 'list-footer';
+
+    const priorityBtn = document.createElement('button');
+    priorityBtn.classList.add('priority-btn', 'high');
+    priorityBtn.textContent = 'High';
+
+    const dueDate = document.createElement('div');
+    dueDate.classList = 'due-date';
+    dueDate.textContent = '9/21';
+
+    listFooter.appendChild(priorityBtn);
+    listFooter.appendChild(dueDate);
+
+    listInfo.appendChild(listTitle);
+    listInfo.appendChild(listDescription);
+    listInfo.appendChild(listFooter);
+
+    listItem.appendChild(checkbox);
+    listItem.appendChild(listInfo);
+
+    listItems.appendChild(listItem);
+
+    list.appendChild(listHeader);
+    list.appendChild(listItems);
+
+    return list;
+}
+
+function loadHome() {
+    const body = document.querySelector('body');
+
+    body.appendChild(loadHeader());
+    body.appendChild(loadMain());
+
+    return body;
+}
+
+export { loadHome };
